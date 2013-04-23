f02eba2f8eba0b1f744344bfc83b6f5ab9cad0ce
PeterHillerstrom <peter.hillerstrom93@gmail.com>
2013-04-23 20:19:41 +0200

    Updated NumberBuilder again. Can now have a number in the middle of a String instead of just the end.

--- src/com/skipifzero/petorsandroidframework/framework/NumberBuilder.java
+++ src/com/skipifzero/petorsandroidframework/framework/NumberBuilder.java
@@ -1,12 +1,13 @@
 package com.skipifzero.petorsandroidframework.framework;
 
+
 /**
  * NumberBuilder: A simple class for building and updating a String with a number.
  * 
  * @author Peter Hillerström
  * @author Centril<twingoow@gmail.com> / Mazdak Farrokhzad.
- * @since 2013-04-07
- * @version 2.0
+ * @since 2013-04-23
+ * @version 3
  */
 public class NumberBuilder {
 	/** The current number. */
@@ -16,18 +17,47 @@ public class NumberBuilder {
 	private final StringBuilder builder;
 	
 	/** The start index when updating the StringBuilder */
-	private final int deleteStart;
+	private final int startOffset;
+	
+	private final String postString;
 	
 	/**
-	 * Constructs the builder with a specified text and a initial number.
-	 * For example: "FPS: " and "30" will give the String "FPS: 30".
-	 *
-	 * @param initialNumber the initial number to set.
+	 * Constructs the builder with the specified texts and a initial number.
+	 * For example: "Money: ", 100, "$" will give the String "Money: 100$" where only the number
+	 * will change when you update the Builder.
+	 * 
+	 * Null is interpreted as an empty string.
+	 * 
+	 * @param preString the String before the number
+	 * @param initialNumber the initial number
+	 * @param postString the String after the number
 	 */
-	public NumberBuilder( String text, int initialNumber ) {
-		this.number = initialNumber;
-		this.builder = new StringBuilder( text + this.number );
-		this.deleteStart = text.length();
+	public NumberBuilder(String preString, int initialNumber, String postString) {
+		builder = new StringBuilder();
+		this.postString = postString;
+		
+		if(preString != null) {
+			builder.append(preString);
+			startOffset = preString.length();
+		} else {
+			startOffset = 0;
+		}
+		builder.append(initialNumber);
+		if(postString != null) {
+			builder.append(postString);
+		}
+	}
+	
+	public NumberBuilder(String preString, int initalNumber) {
+		this(preString, initalNumber, null);
+	}
+	
+	public NumberBuilder(int initialNumber, String postString) {
+		this(null, initialNumber, postString);
+	}
+	
+	public NumberBuilder(int initialNumber) {
+		this(null, initialNumber, null);
 	}
 
 	/**
@@ -35,10 +65,21 @@ public class NumberBuilder {
 	 * 
 	 * @param number the number to set
 	 */
-	public void update( int number ) {
-		if ( this.number != number ) {
+	public void update(int number) {
+		if(this.number != number) {
 			this.number = number;
-			this.builder.delete( deleteStart, builder.length() ).append( this.number );
+			
+			/*
+			 * Unfortunately this bit of code didn't work as expected. But I'll let it remain so
+			 * someone can try to fix it in the future if they ever get the time.
+			 */
+			//builder.delete(startOffset, startOffset + MathUtils.digitsInNumber(number));
+			//builder.insert(startOffset, number);
+			
+			builder.delete(startOffset, builder.length()).append(number);
+			if(postString != null) {
+				builder.append(postString);
+			}
 		}
 	}
 
